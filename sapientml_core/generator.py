f978329dbb58d9099aeb9ea71404c63a19435f8d
Akira Ura <ura.akira@jp.fujitsu.com>
2023-09-01 11:43:31 +0900

    feat: remove temp directory (#5)
    * removed filepath replacement in save
    
    Signed-off-by: Akira Ura <ura.akira@jp.fujitsu.com>
    
    * passed lint
    
    Signed-off-by: Akira Ura <ura.akira@jp.fujitsu.com>
    
    ---------
    
    Signed-off-by: Akira Ura <ura.akira@jp.fujitsu.com>
    Signed-off-by: Kosaku Kimura <kimura.kosaku@fujitsu.com>
    Co-authored-by: Kosaku Kimura <kimura.kosaku@fujitsu.com>

--- sapientml_core/generator.py
+++ sapientml_core/generator.py
@@ -31,7 +31,7 @@ from sapientml.util.logging import setup_logger
 
 from .adaptation.generation.template_based_adaptation import Adaptation
 from .explain.main import process as explain
-from .params import SimplePipeline, SapientMLConfig, summarize_dataset
+from .params import SapientMLConfig, SimplePipeline, summarize_dataset
 from .seeding.predictor import predict
 
 model_dir_path_default = Path(__file__).parent / "models"
@@ -167,25 +167,28 @@ class SapientMLGenerator(PipelineGenerator, CodeBlockGenerator):
         return PipelineResult(score=score, metric=metric, best_params=best_params)
 
     def save(self, output_dir: Union[Path, str]):
-        
         self.output_dir = Path(output_dir)
         self.output_dir.mkdir(parents=True, exist_ok=True)
-
-        _output_dir = Path(self.dataset.output_dir)
 
         candidate_scripts = self._candidate_scripts
         if candidate_scripts:
             if self._best_pipeline:
-                script_body = self._best_pipeline.test.replace(_output_dir.as_posix(), ".")
-                with open(self.output_dir / add_prefix("final_script.py", self.config.project_name), "w", encoding="utf-8") as f:
+                script_body = self._best_pipeline.test
+                with open(
+                    self.output_dir / add_prefix("final_script.py", self.config.project_name), "w", encoding="utf-8"
+                ) as f:
                     f.write(script_body)
 
-                script_body = self._best_pipeline.train.replace(_output_dir.as_posix(), ".")
-                with open(self.output_dir / add_prefix("final_train.py", self.config.project_name), "w", encoding="utf-8") as f:
+                script_body = self._best_pipeline.train
+                with open(
+                    self.output_dir / add_prefix("final_train.py", self.config.project_name), "w", encoding="utf-8"
+                ) as f:
                     f.write(script_body)
 
-                script_body = self._best_pipeline.predict.replace(_output_dir.as_posix(), ".")
-                with open(self.output_dir / add_prefix("final_predict.py", self.config.project_name), "w", encoding="utf-8") as f:
+                script_body = self._best_pipeline.predict
+                with open(
+                    self.output_dir / add_prefix("final_predict.py", self.config.project_name), "w", encoding="utf-8"
+                ) as f:
                     f.write(script_body)
 
                 with open(
@@ -208,10 +211,7 @@ class SapientMLGenerator(PipelineGenerator, CodeBlockGenerator):
                     copyfile(file, lib_path / Path(file).name)
 
             for index, (script, detail) in enumerate(candidate_scripts, start=1):
-                # script.dataset.training_data_path is '{user specified dir}/{name}.csv' or '{tmpdir}/training.pkl'
-                # If latter one, we have to modify the {tmpdir} to output_dir.
-                script_body = script.validation.replace(_output_dir.as_posix(), ".")
-
+                script_body = script.validation
                 with open(self.output_dir / f"{index}_script.py", "w", encoding="utf-8") as f:
                     f.write(script_body)
 
@@ -221,7 +221,9 @@ class SapientMLGenerator(PipelineGenerator, CodeBlockGenerator):
             self.debug_info[i] = info
 
         if self.config.debug:
-            with open(self.output_dir / add_prefix("run_info.json", self.config.project_name), "w", encoding="utf-8") as f:
+            with open(
+                self.output_dir / add_prefix("run_info.json", self.config.project_name), "w", encoding="utf-8"
+            ) as f:
                 json.dump(self.debug_info, f, cls=JSONEncoder, indent=4)
 
         if self.config.add_explanation:
