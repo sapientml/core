6ca1fb0741ca7bf946d7119ea2940ef9ada589b4
Mehdi Bahrami <{ID}+{username}@users.noreply.github.com>
2023-11-10 16:26:42 -0800

    export template

--- sapientml_core/adaptation/generation/pipeline_template.py
+++ sapientml_core/adaptation/generation/pipeline_template.py
@@ -167,6 +167,9 @@ class PipelineTemplate(BaseModel):
         pipeline.pipeline_json["target_separation"]["code_train"] = self._render(tpl, pipeline=pipeline)
         tpl = env.get_template("other_templates/target_separation_predict.py.jinja")
         pipeline.pipeline_json["target_separation"]["code_predict"] = self._render(tpl, pipeline=pipeline)
+        if pipeline.task.export_preprocess_dataset:
+            tpl = env.get_template("other_templates/preprocess_dataset.py.jinja")
+            pipeline.pipeline_json["preprocess_dataset"]["code_test"] = self._render(tpl, pipeline=pipeline)
 
         flag_hyperparameter_tuning = (
             pipeline.config.hyperparameter_tuning and model_name not in NO_TUNABLE_PARAMS_MODELS
@@ -193,7 +196,6 @@ class PipelineTemplate(BaseModel):
 
         tpl = env.get_template("other_templates/prediction_result.py.jinja")
         pipeline.pipeline_json["output_prediction"]["code"] = self._render(tpl, pipeline=pipeline, macros=macros)
-        pipeline.pipeline_json["output_prediction"]["code_test"] = self._render(tpl, pipeline=pipeline, macros=macros)
 
         if flag_hyperparameter_tuning:
             tpl = env.get_template("model_templates/hyperparameters.py.jinja")
@@ -216,18 +218,6 @@ class PipelineTemplate(BaseModel):
             )
 
         self.populate_model()
-
-        if pipeline.adaptation_metric and (
-            pipeline.adaptation_metric in macros.metric_needing_predict_proba
-            or pipeline.adaptation_metric.startswith(macros.Metric.MAP_K.value)
-            or pipeline.config.predict_option == macros.PRED_PROBABILITY
-        ):
-            pipeline.pipeline_json["evaluation"]["code_test"] = pipeline.pipeline_json["evaluation"][
-                "code_test"
-            ].replace("y_pred", "y_prob")
-            pipeline.pipeline_json["output_prediction"]["code_test"] = pipeline.pipeline_json["output_prediction"][
-                "code_test"
-            ].replace("y_pred", "y_prob")
 
         if pipeline.config.permutation_importance:
             tpl = env.get_template("other_templates/permutation_importance.py.jinja")
@@ -457,7 +447,6 @@ class PipelineTemplate(BaseModel):
         tpl = env.get_template("model_templates/model.py.jinja")
         tpl_train = env.get_template("model_templates/model_train.py.jinja")
         tpl_predict = env.get_template("model_templates/model_predict.py.jinja")
-        tpl_test = env.get_template("model_templates/model_test.py.jinja")
 
         flag_no_random_seed_model = model_name in NO_RANDOM_SEED_MODELS
         flag_hyperparameter_tuning = (
@@ -518,18 +507,6 @@ class PipelineTemplate(BaseModel):
             is_multioutput_classification=_is_multioutput_classification,
             metric_needing_predict_proba=macros.metric_needing_predict_proba,
         )
-        snippet_test = self._render(
-            tpl_test,
-            pipeline=pipeline,
-            import_library=MODEL_IMPORT_LIBRARY_MAP[model_name],
-            model_name=model_name,
-            params=pipeline.model.hyperparameters or "",
-            model_arg=model_arg,
-            flag_predict_proba=flag_predict_proba,
-            is_multioutput_regression=_is_multioutput_regression,
-            is_multioutput_classification=_is_multioutput_classification,
-            metric_needing_predict_proba=macros.metric_needing_predict_proba,
-        )
 
         # change "predict" to "predict_proba", e.g., for metric = LogLoss, ROC_AUC, Gini since they require probability to be calculated
         if pipeline.adaptation_metric and (
@@ -545,13 +522,9 @@ class PipelineTemplate(BaseModel):
             tpl_predict = env.get_template("model_templates/classification_post_process.jinja")
             snippet_predict += "\n" + self._render(tpl_predict, pipeline=pipeline)
 
-            tpl_test = env.get_template("model_templates/classification_post_process.jinja")
-            snippet_test += "\n" + self._render(tpl_test, pipeline=pipeline).replace("y_pred", "y_prob")
-
         model_component_json["code"] = snippet
         model_component_json["code_train"] = snippet_train
         model_component_json["code_predict"] = snippet_predict
-        model_component_json["code_test"] = snippet_test
 
         with open(Path(os.path.dirname(__file__)) / "../../models/feature_importance.json", "r", encoding="utf-8") as f:
             model_feature_weights = json.load(f)
