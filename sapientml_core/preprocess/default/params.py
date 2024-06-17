cd41fd1fc7e3213d03d53a8f126862424396bdfa
Arima, Tsukasa <133741487+arima-tsukasa@users.noreply.github.com>
2024-06-17 23:43:43 +0900

    feat: rewrite parameter validation using types (#71)
    * feat: rewrite parameter validation using types
    
    Signed-off-by: arima-tsukasa <fj8215cz@fujitsu.com>
    
    * test: Maintain the test code.
    
    Signed-off-by: arima-tsukasa <fj8215cz@fujitsu.com>
    
    * feat: rewrite parameter validation using types
    
    Signed-off-by: arima-tsukasa <fj8215cz@fujitsu.com>
    
    ---------
    
    Signed-off-by: arima-tsukasa <fj8215cz@fujitsu.com>
    Co-authored-by: Kosaku Kimura <kimusaku@gmail.com>

--- sapientml_core/preprocess/default/params.py
+++ sapientml_core/preprocess/default/params.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sapientml.params import Config
+from sapientml.params import Config, String
 
 
 class DefaultPreprocessConfig(Config):
@@ -19,5 +19,5 @@ class DefaultPreprocessConfig(Config):
 
     """
 
-    use_pos_list: Optional[list[str]] = ["名詞", "動詞", "助動詞", "形容詞", "副詞"]
+    use_pos_list: Optional[list[String]] = ["名詞", "動詞", "助動詞", "形容詞", "副詞"]
     use_word_stemming: bool = True
